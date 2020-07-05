29ac90a04015a1ef020326d6e1d563ab1fa51feb
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-07-05 19:42:23 -0400

    CRUD for Inbox and Messages

--- app/controllers/inboxes_controller.rb
+++ app/controllers/inboxes_controller.rb
@@ -1,2 +1,44 @@
 class InboxesController < ApplicationController
+    def index
+        inboxes = Inbox.all
+        render json: inboxes
+    end
+    
+    def new
+        inbox = Inbox.new(params[:id])
+        render json: inboxs
+    end
+    
+    def create
+        inbox = Inbox.create(inbox_params)
+        render json: inbox
+    end
+    
+    def edit
+        inbox = Inbox.find(params[:id])
+        render json: inbox
+    end
+    
+    def update
+        inbox = Inbox.find(params[:id])
+        inbox.update(inbox_params)
+        render json: inbox
+    end
+    
+    def show
+        inbox = Inbox.find(params[:id])
+        render json: inbox
+    end
+    
+    def destroy
+        Inbox.destroy
+        render json: inbox
+    end
+
+
+    private
+    def inbox_params
+        params.require(inbox).permit(:user_id, :message_id)
+    end
+
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,43 @@
 class MessagesController < ApplicationController
+    def index
+        messages = Message.all
+        render json: messages
+    end
+    
+    def new
+        message = Message.new(params[:id])
+        render json: messages
+    end
+    
+    def create
+        message = Message.create(message_params)
+        render json: message
+    end
+    
+    def edit
+        message = Message.find(params[:id])
+        render json: message
+    end
+    
+    def update
+        message = Message.find(params[:id])
+        message.update(message_params)
+        render json: message
+    end
+    
+    def show
+        message = Message.find(params[:id])
+        render json: message
+    end
+    
+    def destroy
+        Message.destroy
+        render json: message
+    end
+
+
+    private
+    def message_params
+        params.require(message).permit(:message_title, :message_body)
+    end
 end
